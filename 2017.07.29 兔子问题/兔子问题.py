71fe0310301680577fb376cac7430ec4754fbdac
FoxGeeker <fengyunkkx@qq.com>
2017-07-29 17:02:02 +0800

    兔子问题和汉诺塔问题
    兔子问题和汉诺塔问题

---
+++ 2017.07.29 兔子问题/兔子问题.py
@@ -0,0 +1,38 @@
+﻿#2017.07.29 兔子问题
+#迭代实现
+
+def fab(n):
+       a1 = 1
+       a2 = 1
+       a3 = 1
+
+       if n < 1:
+              print('输入有误！')
+              return -1
+       while (n-2) > 0:
+              a3 = a1 + a2
+              a1 = a2
+              a2 = a3
+              n -= 1
+       return a3
+
+result = fab(35)
+if result != -1 :
+       print('总共有 %d 对小兔子诞生！' %result)
+
+
+
+#递归实现
+'''
+def fab(n):
+       if n < 1:
+              print('输入错误！')
+              return -1
+       if n == 1 or n == 2:
+              return 1
+       else:
+              return fab(n-1) + fab(n-2)
+result = fab(35)
+if result != -1:
+       print ('总共有 %d 对小兔子诞生！' %result)
+'''
---
+++ 2017.07.29 汉诺塔问题/汉诺塔.py
@@ -0,0 +1,10 @@
+﻿# 利用递归 解决汉诺塔问题
+def hanoi(n,x,y,z):
+    if n == 1:
+        print(x,'-->',z) #如果只有一层，直接从 x 移动到 z 上
+    else:
+        hanoi(n-1,x,z,y) #将前 n-1 个盘子从 x 移动到 y 上
+        print(x,'-->',y) #将最下面的第 64 个盘子从 x 移动到 z 上
+        hanoi(n-1,y,x,z) #将 y 上的 63个盘子移动到 z 上
+n = int(input('请输入汉诺塔的层数：'))
+hanoi(n,'X','Y','Z')
