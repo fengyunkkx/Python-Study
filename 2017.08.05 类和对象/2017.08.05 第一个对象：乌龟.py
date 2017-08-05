6dfc529db4601e38621cc963929aaad429ebe41c
FoxGeeker <fengyunkkx@qq.com>
2017-08-05 21:07:42 +0800

    类和对象
    类和对象

---
+++ 2017.08.05 类和对象/2017.08.05 第一个对象：乌龟.py
@@ -0,0 +1,18 @@
+class Turtle:
+    color = 'green'
+    weight = 10
+    legs = 4
+    shell = True
+    mouth = '大嘴'
+
+    def climb(self):
+        print("我正在很努力地往前爬")
+    def run(self):
+        print("我正在飞快地往前跑")
+    def bite(self):
+        print("咬你")
+    def eat(self):
+        print("有得吃，真幸福")
+    def sleep(self):
+        print("困了，晚安Zzz")
+        
---
+++ 2017.08.05 类和对象/2017.08.05 鱼类的继承-改进.py
@@ -0,0 +1,62 @@
+import random as r
+
+class Fish:
+    def __init__(self):
+        self.x = r.randint(0,10)
+        self.y = r.randint(0,10)
+
+    def move(self):
+        self.x -= 1
+        print("我的位置在：",self.x, self.y)
+
+class Goldfish(Fish):
+    pass
+
+class Carp(Fish):
+    pass
+
+class Salmon(Fish):
+    pass
+
+class Shark(Fish):
+    def __init__(self):
+        super().__init__()
+        self.hungry = True
+    def eat(self):
+        if self.hungry:
+            print("吃货的梦想就是天天有得吃！")
+            self.hungry = False
+        else:
+            print("太撑了，吃不下了")
+
+'''
+测试
+>>> fish = Fish()
+>>> #试试看小鱼能不能移动
+>>> fish.move()
+我的位置在： 6 3
+>>> goldfish = Goldfish()
+>>> goldfish.move()
+我的位置在： 1 7
+>>> goldfish.move()
+我的位置在： 0 7
+>>> goldfish.move()
+我的位置在： -1 7
+>>>
+>>> #可见金鱼确实是在一路向西
+>>>
+>>>
+>>> #下面生成鲨鱼
+>>> shark = Shark()
+>>> # 试试看能不能吃东西
+>>> shark.eat()
+吃货的梦想就是天天有得吃！
+>>> shark.move()
+我的位置在： 9 3
+>>> shark.move()
+我的位置在： 8 3
+>>>
+
+
+
+'''
